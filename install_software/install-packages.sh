97d797e831e92685fac3065693257db302dd8184
Levi Noeninckx <levi.noenincx@gmail.com>
2022-03-18 00:45:36 +0100

    add alacritty to install

--- install_software/install-packages.sh
+++ install_software/install-packages.sh
@@ -4,3 +4,4 @@ sudo pacman -S krita --noconfirm
 sudo pacman -S obs-studio --noconfirm
 sudo pacman -S kdenlive --noconfirm
 sudo pacman -S visual-studio-code-bin --noconfirm
+sudo pacman -S alacritty --noconfirm
